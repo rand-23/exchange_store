d687d0a7e72a831da7e27724396469a8e2cebf1d
rand-23 <randshamsaldin01@gmail.com>
2025-06-14 16:01:20 +0300

    Update exchange_store.py

--- exchange_store.py
+++ exchange_store.py
@@ -13,26 +13,31 @@ To = input("Exchange To(USD, EUR, SAR): ").upper()
 NewAmount = 0
 if From == To:
     print("You will keep your amount as it is: ", Amount, From)
-    exit()
+    
 elif From == "USD" and To == "EUR":
     NewAmount = Amount * EUR
+    print("You will give: " + str(Amount) + From + ", and you will take: " + str(NewAmount) + To)
    
 elif From == "USD" and To == "SAR":
     NewAmount = Amount * SAR
+    print("You will give: " + str(Amount) + From + ", and you will take: " + str(NewAmount) + To)
 
 elif From == "EUR" and To == "USD":
     NewAmount = Amount / EUR
+    print("You will give: " + str(Amount) + From + ", and you will take: " + str(NewAmount) + To)
 
 elif From == "EUR" and To == "SAR":    
     NewAmount = Amount / EUR * SAR
+    print("You will give: " + str(Amount) + From + ", and you will take: " + str(NewAmount) + To)
 
 elif From == "SAR" and To == "USD":
-     NewAmount = Amount / SAR
+    NewAmount = Amount / SAR
+    print("You will give: " + str(Amount) + From + ", and you will take: " + str(NewAmount) + To)
 
 elif From == "SAR" and To == "EUR":
- NewAmount = Amount / SAR *EUR
+    NewAmount = Amount / SAR *EUR
+    print("You will give: " + str(Amount) + From + ", and you will take: " + str(NewAmount) + To)
 
 else:
     print("You wrote wrong currancy!")
-    exit()
-print("You will give: " + str(Amount) + From + ", and you will take: " + str(NewAmount) + To)
+   
